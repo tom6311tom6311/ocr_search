c19cc4e5f42cf8933aa37281773dd21bf44408a0
Tom Huang <tom6311tom6311@gmail.com>
2019-10-21 14:53:30 +0800

    feat: integrate with jest testing framework; update packages
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  "env": {
+    "jest": true,
+  },
   "extends": "airbnb-base",
   "plugins": [
       "import"
---
+++ src/DropboxSynchronizer/DropboxSynchronizer.test.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import rmrf from 'rimraf';
+import DropboxSynchronizer from './DropboxSynchronizer.class';
+import AppConfig from '../../config/AppConfig.const';
+
+const cleanDataDir = () => {
+  rmrf.sync(AppConfig.PATHS.PPTX_DIR);
+  rmrf.sync(AppConfig.PATHS.PDF_DIR);
+  rmrf.sync(AppConfig.PATHS.PNG_DIR);
+};
+
+afterAll(() => cleanDataDir());
+
+test('local folder hierarchy is constructed when the synchronizer is accessed for the first time', () => {
+  expect(fs.existsSync(AppConfig.PATHS.PPTX_DIR)).toBeTruthy();
+  expect(fs.existsSync(AppConfig.PATHS.PDF_DIR)).toBeTruthy();
+  expect(fs.existsSync(AppConfig.PATHS.PNG_DIR)).toBeTruthy();
+});
+
+test('the synchronizer can list files of remote folder', (done) => {
+  DropboxSynchronizer.dbx.filesListFolder({
+    path: '',
+    recursive: true,
+  })
+    .then(({ entries }) => {
+      expect(entries).toBeInstanceOf(Array);
+      done();
+    });
+});
