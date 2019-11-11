d5a268d699c7b0a853cdcb8c64ae953e37c93e24
Tom Huang <tom6311tom6311@gmail.com>
2019-11-11 20:03:42 +0800

    feat: documentation of ApiHandler
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- src/ApiServer/ApiHandler.const.js
+++ src/ApiServer/ApiHandler.const.js
@@ -3,10 +3,17 @@ import TermMatcher from '../TermMatcher/TermMatcher.class';
 import TermExtractor from '../TermExtractor/TermExtractor.class';
 import AppConfig from '../../config/AppConfig.const';
 
+/**
+ * A list of API handlers defining how server should response given a http request.
+ * The API reference can be found in README
+ */
 const ApiHandler = [
   {
     method: 'get',
     path: '/pages',
+    /**
+     * The search-pages API letting user search for pages by keywords
+     */
     handlers: [
       (req, res) => {
         const { searchTerm: query, maxReturn } = req.query;
@@ -15,6 +22,8 @@ const ApiHandler = [
         } else if (maxReturn !== undefined && !new RegExp(/^[1-9]\d*$/).test(maxReturn)) {
           res.status(400).send({ message: 'maxReturn should be a positive integer' });
         } else {
+          // extract terms from query and then match them with pages in the DB.
+          // Return a list of related pages, sorted by correlation (from high to low)
           TermExtractor
             .extractFromQuery(query)
             .then((searchTerms) => TermMatcher.match(searchTerms))
@@ -34,6 +43,9 @@ const ApiHandler = [
   {
     method: 'use',
     path: '/pageImg',
+    /**
+     * The get-page-image API letting user download page image given a path returned by the search-pages API
+     */
     handlers: [
       express.static('data/png'),
     ],
