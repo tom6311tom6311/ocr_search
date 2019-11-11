9de56c6bba650c0fc05b3234465e42d7998f8e53
Tom Huang <tom6311tom6311@gmail.com>
2019-11-11 21:32:14 +0800

    feat: documentation of TermExtractor
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- src/TermExtractor/TermExtractor.class.js
+++ src/TermExtractor/TermExtractor.class.js
@@ -6,12 +6,37 @@ import TaskQueueManager from '../util/TaskQueueManager.class';
 import AppConfig from '../../config/AppConfig.const';
 import PathConvert from '../util/PathConvert.const';
 
+/**
+ * An instance handling extraction of terms from file pages or user query
+ * @param {TaskQueueManager} taskManager a task manager making sure that tasks are done one after another
+ * @param {PDFExtract} pdfExtract a instance handling extraction of text from pdf files. [>> ref <<](https://www.npmjs.com/package/pdf.js-extract)
+ */
 class TermExtractor {
   constructor() {
     this.taskManager = new TaskQueueManager(AppConfig.TEXT_EXTRACT.TIMEOUT);
     this.pdfExtract = new PDFExtract();
   }
 
+  /**
+   * Term extraction from a pdf file
+   * @param {string} pdfPath path of the source pdf file
+   * @returns {Promise<{ pages }>}
+   *
+   * @example
+   * // each entry of "pages" is in following structure
+   * {
+   *   fileId,                    // an identifier generated from the raw file path (i.e. if "test.pdf" is generated from "test.docx", they share the same fileId)
+   *   docId,                     // an identifier of the document (page)
+   *   oriFilePath,               // original file path
+   *   pageIdx,                   // page index (starting from 1)
+   *   imgPath,                   // path of the corresponding png file. This is for front-end application to retrieve image
+   *   termFreqDict: {            // a mapping from terms to their occurrences (term frequency) in the page
+   *     [term]: term_frequency,
+   *     ...
+   *   },
+   * }
+   *
+   */
   extractFromPdf(pdfPath) {
     return new Promise((resolve, reject) => {
       this.taskManager.registerTask({
@@ -21,9 +46,11 @@ class TermExtractor {
           const pngDirPath = PathConvert.pdf.toPngDir(pdfPath);
           let oriFilePath = pdfPath.substring(AppConfig.PATHS.PDF_DIR.length + 1);
 
+          // if the corresponding pptx or docx file exists, it is the original file
           if (fs.existsSync(pptxPath)) oriFilePath = pptxPath.substring(AppConfig.PATHS.PPTX_DIR.length + 1);
-          else if (fs.existsSync(docxPath)) docxPath.substring(AppConfig.PATHS.DOCX_DIR.length + 1);
+          else if (fs.existsSync(docxPath)) oriFilePath = docxPath.substring(AppConfig.PATHS.DOCX_DIR.length + 1);
 
+          // extract text from pdf
           this.pdfExtract.extract(
             pdfPath,
             {},
@@ -35,6 +62,9 @@ class TermExtractor {
                 const fileId = crypto.createHash('sha256').update(oriFilePath).digest('hex');
                 const { pages: rawPages } = data;
                 const pages = [];
+                // for each of the parsed page, strip strange characters and leading/lagging spaces;
+                // then convert to lower case and perform tokenization and stemming on it by calling
+                // "src/py/tokenize_and_stem.py"
                 rawPages.forEach(({ content }, pageIdx) => {
                   const text = content
                     .map(({ str }) => str.replace(/|•|、/g, '').replace(/^ +| +$/g, ''))
@@ -44,10 +74,14 @@ class TermExtractor {
                   const bufs = [];
                   const tokenizingProcess = spawn('python3', ['src/py/tokenize_and_stem.py', text]);
                   tokenizingProcess.stdout.on('data', (buf) => {
+                    // push the received buffer to an array instead of parse it immediately.
+                    // this is to avoid the case that output data size exceeds stdout limit
+                    // and being segmented forcibly
                     bufs.push(buf);
                   });
                   tokenizingProcess.on('close', () => {
                     try {
+                      // concatenate the buffers and try to parse it as a JSON string
                       const termFreqDict = JSON.parse(Buffer.concat(bufs));
                       pages.push({
                         fileId,
@@ -60,12 +94,15 @@ class TermExtractor {
                         imgPath: `${pngDirPath}/p-${(pageIdx + 1).toString().padStart(rawPages.length.toString().length, '0')}.png`,
                         termFreqDict,
                       });
+                      // if all pages are parsed successfully, resolve the promise with the parsed "pages"
                       if (pages.length === rawPages.length) {
                         resolve({ pages });
                         cb();
                       }
                     } catch (err) {
                       console.log(`ERROR [TermExtractor]: ${err}`);
+                      reject();
+                      cb();
                     }
                   });
                 });
@@ -81,13 +118,21 @@ class TermExtractor {
     });
   }
 
+  /**
+   * Term extraction from a user query string
+   * @param {string} query given by user
+   * @returns {Array<string>} an array of extracted terms
+   */
   extractFromQuery(query) {
     return new Promise((resolve, reject) => {
       this.taskManager.registerTask({
         job: (cb) => {
+          // strip strange characters and leading/lagging spaces; then convert to lower case
+          // and perform tokenization and stemming on it by calling "src/py/tokenize_and_stem.py"
           const text = query.replace(/|•|、/g, '').replace(/^ +| +$/g, '').toLowerCase();
           const tokenizingProcess = spawn('python3', ['src/py/tokenize_and_stem.py', text]);
           tokenizingProcess.stdout.on('data', (buf) => {
+            // try to parse the returned buffer as a JSON string
             const termFreqDict = JSON.parse(buf.toString());
             resolve(Object.keys(termFreqDict));
             cb();
@@ -106,7 +151,8 @@ class TermExtractor {
     });
   }
 
-  // extractFromPng(pngDirPath, callback = () => {}, failCallback = () => {}) {}
+  // TODO: the OCR way of text extraction
+  // extractFromPng(pngDirPath) {}
 }
 
 export default new TermExtractor();
