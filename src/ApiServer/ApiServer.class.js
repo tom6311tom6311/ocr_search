650991e70ac9ab33a482c972808ab13c6ba5c9b5
Tom Huang <tom6311tom6311@gmail.com>
2019-11-11 16:36:42 +0800

    feat: documentation of ApiServer
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- src/ApiServer/ApiServer.class.js
+++ src/ApiServer/ApiServer.class.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import ApiHandler from './ApiHandler.const';
 
+/**
+ * An API server instance taking care of handling client requests
+ * @param {object} app an express server instance
+ */
 class ApiServer {
   constructor() {
     this.app = null;
   }
 
+  /**
+   * start the API server
+   * @param {number} port the port on which the API server will listen
+   */
   start(port) {
     this.app = express();
     this.app.use(express.json());
