103ad0ae15dae0ba580878276bd1245ae8aae7b4
Tom Huang <tom6311tom6311@gmail.com>
2019-11-11 20:23:36 +0800

    feat: documentation of TypeConverter
    Signed-off-by: Tom Huang <tom6311tom6311@gmail.com>

--- src/TypeConverter/TypeConverter.class.js
+++ src/TypeConverter/TypeConverter.class.js
@@ -7,11 +7,20 @@ import TaskQueueManager from '../util/TaskQueueManager.class';
 import AppConfig from '../../config/AppConfig.const';
 import PathConvert from '../util/PathConvert.const';
 
+/**
+ * An instance handling the conversion between several file types: `pptx, docx, pdf, png`
+ * @param {TaskQueueManager} convertManager a task manager making sure that tasks are done one after another
+ */
 class TypeConverter {
   constructor() {
     this.convertManager = new TaskQueueManager(AppConfig.TYPE_CONVERT.TIMEOUT);
   }
 
+  /**
+   * Conversion from pptx to pdf
+   * @param {string} pptxPath path of the source pptx file
+   * @returns {Promise<any>}
+   */
   pptx2pdf(pptxPath) {
     return new Promise((resolve, reject) => {
       this.convertManager.registerTask({
@@ -20,14 +29,17 @@ class TypeConverter {
           const pptxFile = fs.readFileSync(pptxPath);
           const pdfPath = PathConvert.pptx.toPdf(pptxPath);
 
+          // if the target file already exists, remove it
           if (fs.existsSync(pdfPath)) {
             fs.unlinkSync(pdfPath);
           }
 
+          // if the directory where the target file should be placed does not exist, construct it
           if (!fs.existsSync(path.dirname(pdfPath))) {
             fs.mkdirSync(path.dirname(pdfPath), { recursive: true });
           }
 
+          // convert and save to the corresponding pdf path
           toPdf(pptxFile).then(
             (pdfFile) => {
               fs.writeFileSync(pdfPath, pdfFile);
@@ -48,6 +60,11 @@ class TypeConverter {
     });
   }
 
+  /**
+   * Conversion from docx to pdf
+   * @param {string} docxPath path of the source docx file
+   * @returns {Promise<any>}
+   */
   docx2pdf(docxPath) {
     return new Promise((resolve, reject) => {
       this.convertManager.registerTask({
@@ -56,14 +73,17 @@ class TypeConverter {
           const docxFile = fs.readFileSync(docxPath);
           const pdfPath = PathConvert.docx.toPdf(docxPath);
 
+          // if the target file already exists, remove it
           if (fs.existsSync(pdfPath)) {
             fs.unlinkSync(pdfPath);
           }
 
+          // if the directory where the target file should be placed does not exist, construct it
           if (!fs.existsSync(path.dirname(pdfPath))) {
             fs.mkdirSync(path.dirname(pdfPath), { recursive: true });
           }
 
+          // convert and save to the corresponding pdf path
           toPdf(docxFile).then(
             (pdfFile) => {
               fs.writeFileSync(pdfPath, pdfFile);
@@ -84,24 +104,27 @@ class TypeConverter {
     });
   }
 
+  /**
+   * Conversion from pdf to png
+   * @param {string} pdfPath path of the source pdf file
+   * @returns {Promise<any>}
+   */
   pdf2png(pdfPath) {
     return new Promise((resolve, reject) => {
       this.convertManager.registerTask({
         job: (cb) => {
           console.log(`INFO [TypeConverter]: converting ${pdfPath} to png...`);
-          const pngDir = pdfPath
-            .replace(AppConfig.PATHS.PDF_DIR, AppConfig.PATHS.PNG_DIR)
-            .replace('.pdf', '');
+          const pngDirPath = PathConvert.pdf.toPngDir(pdfPath);
 
-          if (fs.existsSync(pngDir)) {
-            rmrf.sync(pngDir);
+          // if the target png directory already exists, remove it
+          if (fs.existsSync(pngDirPath)) {
+            rmrf.sync(pngDirPath);
           }
+          // construct the target png directory
+          fs.mkdirSync(pngDirPath, { recursive: true });
 
-          if (!fs.existsSync(pngDir)) {
-            fs.mkdirSync(pngDir, { recursive: true });
-          }
-
-          const pdfToPngProcess = spawn('pdftoppm', ['-png', pdfPath, `${pngDir}/p`]);
+          // call the "pdftoppm" command line tool to perform type conversion
+          const pdfToPngProcess = spawn('pdftoppm', ['-png', pdfPath, `${pngDirPath}/p`]);
 
           pdfToPngProcess.stdout.on('data', (data) => {
             console.log(`INFO [TypeConverter]: ${data}`);
@@ -112,6 +135,7 @@ class TypeConverter {
           });
 
           pdfToPngProcess.on('close', (code) => {
+            // verify conversion result by checking exist code
             switch (code) {
               case 0: // no error
                 resolve();
@@ -123,7 +147,7 @@ class TypeConverter {
                 cb();
                 break;
               case 2: // error opening an output file
-                console.log(`ERROR [TypeConverter]: error opening png files under "${pngDir}"`);
+                console.log(`ERROR [TypeConverter]: error opening png files under "${pngDirPath}"`);
                 reject();
                 cb();
                 break;
